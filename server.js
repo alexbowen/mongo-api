52499e4dcf8b7747220aa423f98719f96f62b59c
Alex Bowen <alexbowen123@googlemail.com>
2014-07-10 23:06:57 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
+
+app.get('/api/:type', function(req, res){
+
+	console.log('table', req.param('type'));
+
+	MongoClient.connect('mongodb://127.0.0.1:27017/ttgd', function(err, db) {
+	    if(err) throw err;
+
+	    var collection = db.collection('ttgd');
+
+	    // Locate all the entries using find
+	    collection.find({type:req.param('type')}).toArray(function(err, results) {
+
+	    	res.header('Access-Control-Allow-Origin', "*");
+	        res.send(results);
+	        // Let's close the db
+	        db.close();
+	    });
+	});
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
