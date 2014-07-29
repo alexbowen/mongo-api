450d1b175b3f367dddad2b9472deb4d1d6a51f08
Alex Bowen <alexbowen123@googlemail.com>
2014-07-29 08:54:57 +0100

    added google api

--- server.js
+++ server.js
@@ -2,6 +2,54 @@ var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+var port = 8001;
+
+var options = {
+    key: fs.readFileSync('./myrsakey.pem'),
+    cert: fs.readFileSync('./myrsacert.pem'),
+};
+
+var app2 = express();
+
+var sserver = https.createServer(options, app2).listen(port, function(){
+  console.log("Express server listening on port " + port);
+});
+
+app.get('/blog', function(req, res){
+
+	console.log('/blogger/v3/blogs/888871340674436854/posts?key=AIzaSyBGIwF36zQGgvyDkA1kh6B5H8JqIcAEUE4&labels=' + req.param('label'));
+
+	var options = {
+	  host: 'www.googleapis.com',
+	  //port: '80',
+	  path: '/blogger/v3/blogs/888871340674436854/posts?key=AIzaSyBGIwF36zQGgvyDkA1kh6B5H8JqIcAEUE4&labels=' + req.param('label'),
+	  method: 'GET'
+	};
+
+//user 888871340674436854
+
+	callback = function(response) {
+	  var str = '';
+
+	  //another chunk of data has been recieved, so append it to `str`
+	  response.on('data', function (chunk) {
+	    str += chunk;
+	  });
+
+	  //the whole response has been recieved, respond to original request here
+	  response.on('end', function () {
+	    res.header('Access-Control-Allow-Origin', "*");
+	    res.send(str);
+	  });
+	}
+
+	https.request(options, callback).end();
+	
+});
 
 app.get('/api/:type', function(req, res){
 
